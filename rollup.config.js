94f120449189453718b352139a304425000a734d
Fritz Lekschas <service@lekschas.de>
2019-05-13 14:35:59 -0400

    Properly setup build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,26 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
-import buble from 'rollup-plugin-buble';
+import commonjs from 'rollup-plugin-commonjs';
 
 const pkg = require('./package.json');
 
 export default {
   input: 'src/Modal.svelte',
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
+    {
+       file: pkg.main,
+       format: 'cjs',
+       exports: 'default'
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'default'
+    },
   ],
   plugins: [
-    buble()
+    svelte(),
+    resolve(),
+    commonjs(),
   ],
 };
