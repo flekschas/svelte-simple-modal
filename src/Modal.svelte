9c2cbf561ddd5ac864f535dd55ab9c4461ea9af7
Fritz Lekschas <service@lekschas.de>
2019-05-14 14:17:09 -0400

    Add close button and increase margin

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -76,12 +76,13 @@
 
   .wrap {
     position: relative;
-    margin: 1rem;
+    margin: 2rem;
     max-height: 100%;
     overflow: auto;
   }
 
   .window {
+    position: relative;
     width: 40rem;
     max-width: 100%;
     margin-left: auto;
@@ -89,6 +90,75 @@
     color: black;
     border-radius: 0.5rem;
     background: white;
+  }
+  .close {
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    margin: 0;
+    padding: 0;
+    width: 1.5rem;
+    height: 1.5rem;
+    border: 0;
+    color: black;
+    border-radius: 1.5rem;
+    background: white;
+    box-shadow: 0 0 0 1px black;
+    transition: transform 0.2s cubic-bezier(0.25, 0.1, 0.25, 1),
+                background 0.2s cubic-bezier(0.25, 0.1, 0.25, 1);
+    -webkit-appearance: none;
+  }
+
+  .close:before, .close:after {
+    content: '';
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    top: 50%;
+    width: 1rem;
+    height: 1px;
+    background: black;
+    transform-origin: center;
+    transition: height 0.2s cubic-bezier(0.25, 0.1, 0.25, 1),
+                background 0.2s cubic-bezier(0.25, 0.1, 0.25, 1);
+  }
+
+  .close:before {
+    -webkit-transform: translate(0, -50%) rotate(45deg);
+    -moz-transform: translate(0, -50%) rotate(45deg);
+    transform: translate(0, -50%) rotate(45deg);
+    left: 0.25rem;
+  }
+
+  .close:after {
+    -webkit-transform: translate(0, -50%) rotate(-45deg);
+    -moz-transform: translate(0, -50%) rotate(-45deg);
+    transform: translate(0, -50%) rotate(-45deg);
+    left: 0.25rem;
+  }
+
+  .close:hover {
+    background: black;
+  }
+
+  .close:hover:before, .close:hover:after {
+    height: 2px;
+    background: white;
+  }
+
+  .close:focus {
+    border-color: #3399ff;
+    box-shadow: 0 0 0 2px #3399ff;
+  }
+
+  .close:active {
+    transform: scale(0.9);
+  }
+
+  .close:hover, .close:focus, .close:active {
+    outline: none;
   }
 
   .content {
@@ -113,6 +183,9 @@
           transition:transitionWindow={transitionWindowProps}
           style={cssWindow}
         >
+          {#if closeButton}
+            <button on:click={close} class="close"></button>
+          {/if}
           <div class="content" style={cssContent}>
             {#if Component}
               <Component {...props}/>
