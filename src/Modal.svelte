flekschas/svelte-simple-modal

commit bad5fd7219b3c9d6a1d62393a38d65288789487d

Update Modal.svelte (#45)
* Update Modal.svelte

fix 'document is not defined'  on sveltekit.

* Default bg width/height to innerWidth/height

Fix #40

* Harmonize code style

* Fix formatting issues...

Co-authored-by: Fritz Lekschas <932103+flekschas@users.noreply.github.com>

src/Modal.svelte
@@ -15,7 +15,7 @@
 <script>
   import * as svelte from 'svelte';
   import { fade } from 'svelte/transition';
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher } from 'svelte';
 
   const dispatch = createEventDispatcher();
 
@@ -82,7 +82,10 @@
   const isFunction = f => !!(f && f.constructor && f.call && f.apply);
 
   const updateStyleTransition = () => {
-    cssBg = toCssString(state.styleBg);
+    cssBg = toCssString(Object.assign({}, {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }, styleBg));
     cssWindowWrap = toCssString(state.styleWindowWrap);
     cssWindow = toCssString(state.styleWindow);
     cssContent = toCssString(state.styleContent);
@@ -106,7 +109,6 @@
     Component = bind(NewComponent, newProps);
     state = { ...defaultState, ...options };
     updateStyleTransition();
-    disableScroll();
     onOpen = (event) => {
       if (callback.onOpen) callback.onOpen(event);
       dispatch('open');
@@ -131,7 +133,6 @@
     onClose = callback.onClose || onClose;
     onClosed = callback.onClosed || onClosed;
     Component = null;
-    enableScroll();
   };
 
   const handleKeydown = (event) => {
@@ -201,6 +202,14 @@
 
   svelte.onDestroy(() => {
     close();
+  });
+
+  svelte.afterUpdate(() => {
+    if (Component) {
+      disableScroll();
+    } else {
+      enableScroll();
+    }
   });
 </script>
 

commit ec8b65a9b03b2706b4e2ed6d2be55e119a027023

Fix: Only call enable/disableScroll on open/close (#47)
* Only call enable/disableScroll once per open/close

Svelte will sometimes update the Modal when variables tracked by its parent (e.g. App) and rendered within the content are updated, meaning enableScroll/disableScroll may be called multiple times. This can cause the page to jump back to the top unexpectedly, and can also cause scrolling to remain disabled after closing the modal.

* Ensure component renders in Vite + SvelteKit

Fixes an issue introduced in #45 

Co-authored-by: Fritz Lekschas <code@lekschas.de>

src/Modal.svelte
@@ -109,6 +109,7 @@
     Component = bind(NewComponent, newProps);
     state = { ...defaultState, ...options };
     updateStyleTransition();
+    disableScroll();
     onOpen = (event) => {
       if (callback.onOpen) callback.onOpen(event);
       dispatch('open');
@@ -133,6 +134,7 @@
     onClose = callback.onClose || onClose;
     onClosed = callback.onClosed || onClosed;
     Component = null;
+    enableScroll();
   };
 
   const handleKeydown = (event) => {
@@ -192,24 +194,24 @@
 
   setContext(key, { open, close });
 
+  let isMounted = false;
+
   $: {
-    if (isFunction(show)) {
-      open(show);
-    } else {
-      close();
+    if (isMounted) {
+      if (isFunction(show)) {
+        open(show);
+      } else {
+        close();
+      }
     }
   }
 
   svelte.onDestroy(() => {
-    close();
+    if (isMounted) close();
   });
 
-  svelte.afterUpdate(() => {
-    if (Component) {
-      disableScroll();
-    } else {
-      enableScroll();
-    }
+  svelte.onMount(() => {
+    isMounted = true;
   });
 </script>
 